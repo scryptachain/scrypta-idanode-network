fc88b5f6bed21f9e19565e72640c7b36268c0392
turinglabsorg <turinglabs@criptext.com>
2020-06-01 09:03:33 +0200

    lint

--- src/main.ts
+++ src/main.ts
@@ -16,15 +16,15 @@ async function bootstrap() {
   await app.listen(3000)
 
   db.createIndex({
-    index: {fields: ['paid']}
+    index: { fields: ['paid'] }
   })
 
   db.createIndex({
-    index: {fields: ['node']}
+    index: { fields: ['node'] }
   })
 
   db.createIndex({
-    index: {fields: ['block']}
+    index: { fields: ['block'] }
   })
 
   checkSystems()
@@ -60,10 +60,10 @@ async function checkSystems() {
         if (check.blocks !== undefined && check.toindex <= 1) {
           console.info('IDANODE IS SYNCED [' + check.toindex + ' BLOCKS]')
           console.log('INTEGRITY CHECK ON ' + node)
-          
+
           let checksumversion = await checkVersion(check.version)
           let integritycheck = await scrypta.get('/wallet/integritycheck', node)
-          
+
           if (checksumversion === integritycheck.checksum) {
             console.info('IDANODE INTEGRITY CHECK PASSED')
 
@@ -124,7 +124,7 @@ async function checkPayouts() {
   } catch (e) {
     console.error('DB IS NOT WORKING')
   }
-  if(dbinfo !== undefined){
+  if (dbinfo !== undefined) {
     let idanodes = await scrypta.returnNodes()
     console.info('CHECKING PAYOUTS')
     let unpaid = await db.find({
@@ -132,15 +132,15 @@ async function checkPayouts() {
         paid: false
       }
     })
-    unpaid.docs.sort(function(a, b){return a.block - b.block})
+    unpaid.docs.sort(function (a, b) { return a.block - b.block })
     let toPay = unpaid.docs
     let payoutByNodes = {}
-    for(let x in toPay){
+    for (let x in toPay) {
       let payout = toPay[x]
-      if(payoutByNodes[payout.node] === undefined){
+      if (payoutByNodes[payout.node] === undefined) {
         payoutByNodes[payout.node] = []
       }
-      if(payout.paid === false){
+      if (payout.paid === false) {
         payoutByNodes[payout.node].push(payout)
       }
     }
@@ -152,60 +152,60 @@ async function checkPayouts() {
 
     console.log('POOL BALANCE IS ' + balance + ' lyra')
     let totpayouts = 0
-    let maxpayouts = 1440    
+    let maxpayouts = 1440
 
-    if(balance !== undefined && balance > 0){
-      for(let k in payoutByNodes){
+    if (balance !== undefined && balance > 0) {
+      for (let k in payoutByNodes) {
         console.info(k + ' HAVE ' + payoutByNodes[k].length + ' UNPAID PAYOUTS')
         totpayouts += payoutByNodes[k].length
       }
-      for(let k in payoutByNodes){
+      for (let k in payoutByNodes) {
         var share = (balance * payoutByNodes[k].length / totpayouts).toFixed(8)
         console.info(k + ' HAVE ' + share + ' LYRA TO EARN')
       }
-      
+
       let midpayouts = totpayouts / idanodes.length
       console.log('MID PAYOUTS ARE ' + midpayouts)
 
-      if(midpayouts >= maxpayouts){
+      if (midpayouts >= maxpayouts) {
         console.log('24H PASSED, DISTRIBUTING SHARES.')
         let nodes_git = await axios.get('https://raw.githubusercontent.com/scryptachain/scrypta-idanode-network/master/peers')
         let raw_nodes = nodes_git.data.split("\n")
-        let unlockwallet = await RPC.request('walletpassphrase',[process.env.WALLET_PASSWORD,999999])
+        let unlockwallet = await RPC.request('walletpassphrase', [process.env.WALLET_PASSWORD, 999999])
 
-        if(unlockwallet['error'] === null){
-          for(let x in raw_nodes){
-              let node = raw_nodes[x].split(':')
-              let url = 'https://idanodejs' + node[0] + '.scryptachain.org'
-              let address = await scrypta.getAddressFromPubKey(node[2])
-              console.info('SENDING PAYOUT TO ' + url + ' -> ADDRESS IS ' + address)
-              let validateaddress = await RPC.request('validateaddress',[address])
-              if(validateaddress['result']['isvalid'] !== undefined && validateaddress['result']['isvalid'] === true){
-                let sharelessfees = parseFloat(share) - 0.003
-                await RPC.request('settxfee', [0.003])
-                let txid = await RPC.request('sendtoaddress', [address, sharelessfees])
-                if(txid['result'] !== undefined && txid['error'] === null){
-                  let payouts = payoutByNodes[url]
-                  for(let xx in payouts){
-                    payouts[xx].paid = true
-                    try{
-                      await db.put(payouts[xx])
-                      console.log('SEND SUCCESS TXID IS: ' + txid['result'])
-                    }catch(e){
-                      console.log(e)
-                    }
+        if (unlockwallet['error'] === null) {
+          for (let x in raw_nodes) {
+            let node = raw_nodes[x].split(':')
+            let url = 'https://idanodejs' + node[0] + '.scryptachain.org'
+            let address = await scrypta.getAddressFromPubKey(node[2])
+            console.info('SENDING PAYOUT TO ' + url + ' -> ADDRESS IS ' + address)
+            let validateaddress = await RPC.request('validateaddress', [address])
+            if (validateaddress['result']['isvalid'] !== undefined && validateaddress['result']['isvalid'] === true) {
+              let sharelessfees = parseFloat(share) - 0.003
+              await RPC.request('settxfee', [0.003])
+              let txid = await RPC.request('sendtoaddress', [address, sharelessfees])
+              if (txid['result'] !== undefined && txid['error'] === null) {
+                let payouts = payoutByNodes[url]
+                for (let xx in payouts) {
+                  payouts[xx].paid = true
+                  try {
+                    await db.put(payouts[xx])
+                    console.log('SEND SUCCESS TXID IS: ' + txid['result'])
+                  } catch (e) {
+                    console.log(e)
                   }
                 }
               }
+            }
           }
-          await RPC.request('walletpassphrase',[process.env.WALLET_PASSWORD,999999999,true])
-        }else{
+          await RPC.request('walletpassphrase', [process.env.WALLET_PASSWORD, 999999999, true])
+        } else {
           console.error('WALLET PASSPHRASE IS WRONG')
         }
-      }else{
+      } else {
         console.log('LESS THAN 24H PASSED.')
       }
-    }else{
+    } else {
       console.log('NOT ENOUGH BALANCE.')
     }
   }
