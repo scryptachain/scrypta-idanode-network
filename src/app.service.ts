a03d43a0dc0df2f4f26b030daf5e5cd840823fae
turinglabsorg <turinglabs@criptext.com>
2020-06-23 11:22:11 +0200

    parse status

--- src/app.service.ts
+++ src/app.service.ts
@@ -6,6 +6,7 @@ let ScryptaCore = require('@scrypta/core')
 let scrypta = new ScryptaCore
 import * as ScryptaRPC from './libs/ScryptaRPC'
 const axios = require('axios')
+const fs = require('fs')
 
 @Injectable()
 export class AppService {
@@ -32,74 +33,8 @@ export class AppService {
 
   async getStatus(): Promise<Object> {
     return new Promise(async response => {
-
-      let dbinfo
-
-      try {
-        dbinfo = await db.info()
-      } catch (e) {
-        response('DB IS NOT WORKING')
-      }
-      if (dbinfo !== undefined) {
-        let RPC = new ScryptaRPC.Wallet
-        let getinfo = await RPC.request('getinfo')
-        let idanodes = await scrypta.returnNodes()
-        let unpaid = await db.find({
-          selector: {
-            paid: false
-          }
-        })
-        unpaid.docs.sort(function (a, b) { return a.block - b.block })
-        let toPay = unpaid.docs
-        let payoutByNodes = {}
-        let payoutByNodesCount = {}
-
-        for (let x in toPay) {
-          let payout = toPay[x]
-          if (payoutByNodes[payout.node] === undefined) {
-            payoutByNodes[payout.node] = []
-          }
-          if (payout.paid === false) {
-            payoutByNodes[payout.node].push(payout)
-          }
-        }
-        let balance = getinfo['result']['balance']
-        let stake = idanodes.length * 5000
-        balance = balance - stake
-
-        if (balance < 0) {
-          balance = 0
-        }
-        let totpayouts = 0
-
-        for (let k in payoutByNodes) {
-          payoutByNodesCount[k] = payoutByNodes[k].length
-          totpayouts += payoutByNodes[k].length
-        }
-        let shares = {}
-        let earnings = {}
-        for (let k in payoutByNodes) {
-          var share = (balance * payoutByNodes[k].length / totpayouts).toFixed(8)
-          earnings[k] = share
-          if (balance > 0) {
-            let percentage = (100 / balance * parseFloat(share)).toFixed(2)
-            shares[k] = parseFloat(percentage)
-            earnings[k] = parseFloat(share) - 0.003
-          } else {
-            shares[k] = 0
-          }
-        }
-
-        let midpayouts = totpayouts / idanodes.length
-
-        response({
-          uptime: payoutByNodesCount,
-          timing: parseFloat(midpayouts.toFixed(0)),
-          earnings: earnings,
-          shares: shares,
-          stake: balance
-        })
-      }
+      let read = fs.readFileSync( './status' )
+      response(JSON.parse(read))
     })
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,7 @@ var PouchDB = require('pouchdb')
 var db = new PouchDB('rewards')
 PouchDB.plugin(require('pouchdb-find'))
 let Crypto = require('crypto')
+const fs = require('fs')
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
@@ -28,10 +29,15 @@ async function bootstrap() {
   })
 
   checkSystems()
+  writeStatus()
 
   setInterval(function () {
     checkSystems()
   }, 60000)
+
+  setInterval(function () {
+    writeStatus()
+  }, 180000)
 }
 
 async function checkSystems() {
@@ -209,6 +215,79 @@ async function checkPayouts() {
       console.log('NOT ENOUGH BALANCE.')
     }
   }
+}
+
+async function writeStatus() {
+  let dbinfo
+
+      try {
+        dbinfo = await db.info()
+      } catch (e) {
+        console.log('DB IS NOT WORKING')
+      }
+
+      if (dbinfo !== undefined) {
+        let RPC = new ScryptaRPC.Wallet
+        let getinfo = await RPC.request('getinfo')
+        let idanodes = await scrypta.returnNodes()
+        let unpaid = await db.find({
+          selector: {
+            paid: false
+          }
+        })
+        unpaid.docs.sort(function (a, b) { return a.block - b.block })
+        let toPay = unpaid.docs
+        let payoutByNodes = {}
+        let payoutByNodesCount = {}
+
+        for (let x in toPay) {
+          let payout = toPay[x]
+          if (payoutByNodes[payout.node] === undefined) {
+            payoutByNodes[payout.node] = []
+          }
+          if (payout.paid === false) {
+            payoutByNodes[payout.node].push(payout)
+          }
+        }
+        let balance = getinfo['result']['balance']
+        let stake = idanodes.length * 5000
+        balance = balance - stake
+
+        if (balance < 0) {
+          balance = 0
+        }
+        let totpayouts = 0
+
+        for (let k in payoutByNodes) {
+          payoutByNodesCount[k] = payoutByNodes[k].length
+          totpayouts += payoutByNodes[k].length
+        }
+        let shares = {}
+        let earnings = {}
+        for (let k in payoutByNodes) {
+          var share = (balance * payoutByNodes[k].length / totpayouts).toFixed(8)
+          earnings[k] = share
+          if (balance > 0) {
+            let percentage = (100 / balance * parseFloat(share)).toFixed(2)
+            shares[k] = parseFloat(percentage)
+            earnings[k] = parseFloat(share) - 0.003
+          } else {
+            shares[k] = 0
+          }
+        }
+
+        let midpayouts = totpayouts / idanodes.length
+
+        let status = {
+          uptime: payoutByNodesCount,
+          timing: parseFloat(midpayouts.toFixed(0)),
+          earnings: earnings,
+          shares: shares,
+          stake: balance
+        }
+        
+        fs.writeFileSync('./status', JSON.stringify(status))
+      }
 }
 
 bootstrap()
